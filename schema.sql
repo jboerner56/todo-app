1afef2d03f587ca19d37f0764e495aade41adfeb
Jason Boerner <jasonboerner@Jasons-MBP.attlocal.net>
2019-03-31 10:56:48 -0400

    adds seed/schema files for database

---
+++ schema.sql
@@ -0,0 +1,6 @@
+create table todo (
+id serial PRIMARY KEY,
+name VARCHAR(50),
+task VARCHAR(200),
+completed VARCHAR(10)
+);
---
+++ seed.sql
@@ -0,0 +1,8 @@
+insert into todo
+(name, task, completed)
+VALUES
+('Jason', 'Laundry', 'No'),
+('Bob,', 'Golf', 'Yes'),
+('Rocky', 'sleep', 'Yes'),
+('Jason', 'Homework', 'No'),
+('Jason', 'Workout', 'Yes')
