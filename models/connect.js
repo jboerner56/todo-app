d074766694d3e98daa5b4305f3c9711be57af4d4
Jason Boerner <jasonboerner@Jasons-MBP.attlocal.net>
2019-03-31 11:08:11 -0400

    sets up connection to database

---
+++ models/connect.js
@@ -0,0 +1,16 @@
+// require pg promise
+const pgp = require('pg-promise')({
+    query: e => {
+    console.log('QUERY: ', e.query);
+    }  
+});
+
+// for connecting the database
+const options = {
+    host: 'localhost',
+    database = 'todo'
+};
+// set pg-promise and options to variable to export
+const db = pgp(options);
+// export so it can be used elsewhere
+module.exports = db;
